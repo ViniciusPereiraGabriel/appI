14eb2d0d5b41e8d3fc77c142e06f93ca710bbe79
Vinicius Gabriel Pereira <89548277+ViniciusPereiraGabriel@users.noreply.github.com>
2022-02-15 16:52:59 -0300

    Add files via upload

---
+++ Atividade1/src/orientacao/Pessoa.java
@@ -0,0 +1,12 @@
+
+package orientacao;
+
+public class Pessoa {
+    public String nome;
+    public Integer idade;
+    public String identidade;
+    public String endereco;
+    public String telefone;
+    //tipos de dados = preto
+    //atributos = verde
+}
---
+++ Atividade1/src/orientacao/main.java
@@ -0,0 +1,17 @@
+
+package orientacao;
+
+public class main {
+    public static void main(String[] args) {
+        Pessoa andreza = new Pessoa();
+        Pessoa nicole = new Pessoa();
+        Pessoa sla = new Pessoa();
+        
+        andreza.nome = "Andreza";
+        andreza.idade = 38;
+        andreza.endereco = "Rua tal das quantas";
+        andreza.identidade = "280990423";
+        andreza.telefone = "999549454";
+        
+    }
+}
